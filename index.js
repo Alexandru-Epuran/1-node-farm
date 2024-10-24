0bbe9d21f360c3dc5b487d27ae445bfccd874f2b
Alexandru Epuran <epuranalex89@gmail.com>
2024-10-24 15:33:58 +0300

    019 Setting up Prettier in VS Code

--- index.js
+++ index.js
@@ -46,42 +46,42 @@ const templateCard = fs.readFileSync(`${__dirname}/starter/templates/template-ca
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
-const slugs = dataObject.map(el => slugify(el.productName, { lower: true }));
+const slugs = dataObject.map((el) => slugify(el.productName, { lower: true }));
 console.log('@@@  slugs@@@', slugs);
 
 const server = http.createServer((req, res) => {
-	const { query, pathname } = url.parse(req.url, true);
+    const { query, pathname } = url.parse(req.url, true);
 
-	//Overview page
-	if (pathname === '/' || pathname === '/overview') {
-		res.writeHead(200, { 'Content-type': 'text/html' });
+    //Overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
 
-		const cardsHtml = dataObject.map(el => replaceTemplate(templateCard, el)).join('');
-		const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        const cardsHtml = dataObject.map((el) => replaceTemplate(templateCard, el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-		res.end(output);
-		//Product page
-	} else if (pathname === '/product') {
-		res.writeHead(200, { 'Content-type': 'text/html' });
-		const product = dataObject[query.id];
-		const output = replaceTemplate(templateProduct, product);
+        res.end(output);
+        //Product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObject[query.id];
+        const output = replaceTemplate(templateProduct, product);
 
-		res.end(output);
-		//API
-	} else if (pathname === '/api') {
-		res.writeHead(200, {
-			'Content-Type': 'application/json',
-		});
-		res.end(data);
-		//Not Found 404
-	} else {
-		res.writeHead(404, {
-			'Content-Type': 'text/html',
-		});
-		res.end('<h1>Page not found</h1>');
-	}
+        res.end(output);
+        //API
+    } else if (pathname === '/api') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        res.end(data);
+        //Not Found 404
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-	console.log('Server is listening on port 8000');
+    console.log('Server is listening on port 8000');
 });
